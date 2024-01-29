2141c80dc4e5d5cd04fd87fee7b99b21ee29d344
Idealisoa04 <119027567+Idealisoa04@users.noreply.github.com>
2024-01-29 14:04:19 +0300

    remake

--- src/main/java/com/CarSelling/project/Config/JwtAuthFilter.java
+++ src/main/java/com/CarSelling/project/Config/JwtAuthFilter.java
@@ -58,9 +58,8 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             if (idUser != null &&
                     SecurityContextHolder.getContext().getAuthentication() == null) {
                         System.out.println(idUser);
-                UtilisateurEntity util = new UtilisateurEntity();
-                util.setIdutilisateur(Integer.valueOf(idUser));
-                
+                UtilisateurEntity util = this.utilisateurService.findUserById(Integer.valueOf(idUser));
+
                 //User user = (User) this.userDetailsService.loadUserByUsername(userEmail);
                 if (jwtService.isTokenValid(jwt, util)) {
                     UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(util,
